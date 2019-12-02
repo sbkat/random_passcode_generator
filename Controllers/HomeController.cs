d3d9078fa48d6fdbc57cf016570248e73926e772
sbkat <kathydangg@gmail.com>
2019-12-01 19:08:24 -0800

    added session

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,19 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using random_passcode.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace random_passcode.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet("")]
         public IActionResult Index()
         {
+            HttpContext.Session.SetInt32("count", 0);
+            int? IntVariable = HttpContext.Session.GetInt32("count");
+            ViewBag.Count = HttpContext.Session.GetInt32("count");
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet("generate")]
+        public IActionResult Generate()
         {
-            return View();
+            return View("");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Startup.cs
+++ Startup.cs
@@ -23,14 +23,7 @@ namespace random_passcode
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
+            services.AddSession();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -47,7 +40,8 @@ namespace random_passcode
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
